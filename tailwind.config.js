74b65a3ccfdaefb144aca5c78f32e663977651fe
Flora <laszlo.t.flora@gmail.com>
2024-03-29 21:08:06 +0100

    mobil view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
+    colors: {
+      red: "hsl(0, 78%, 62%)",
+      cyan: "hsl(180, 62%, 55%)",
+      orange: "hsl(34, 97%, 64%)",
+      blue: "hsl(212, 86%, 64%)",
+      darkBlue: "hsl(234, 12%, 34%)",
+      grayishBlue: "hsl(229, 6%, 66%)",
+      lightGray: "hsl(0, 0%, 98%)",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
 };
-
